2791843fd25c8a1ab324597b38ce3680534aee44
Lucio <luciolopezlecube@gmail.com>
2018-05-15 15:38:25 -0300

    Agrego main con parser

--- TP2/cache.c
+++ TP2/cache.c
@@ -1,12 +1,13 @@
+#include <stdlib.h>
 #include "cache.h"
 #define BLOCK_SIZE 32 // in bytes
 #define CACHE_WAY_SIZE 512 // in bytes
 
 
 void init() {
-	this->cache_1st_way = (int*) malloc(CACHE_WAY_SIZE);
-	this->cache_2nd_way = (int*) malloc(CACHE_WAY_SIZE);
-	this->miss_rate = 0;
+	//this->cache_1st_way = (int*) malloc(CACHE_WAY_SIZE);
+	//this->cache_2nd_way = (int*) malloc(CACHE_WAY_SIZE);
+	//this->miss_rate = 0;
 }
 
 
@@ -17,10 +18,11 @@ int write_byte(int address, unsigned char value);
 
 
 unsigned int get_miss_rate() {
-	return this->miss_rate;
+	//return this->miss_rate;
+    return 0;
 }
 
 void destroy_cache() {
-	free(this->cache_1st_way);
-	free(this->cache_2nd_way);
+	//free(this->cache_1st_way);
+	//free(this->cache_2nd_way);
 }
