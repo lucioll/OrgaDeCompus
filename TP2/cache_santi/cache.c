552b8e427595b740b6442a6d28390bcf6d6cadf5
santiago-alvarezjulia <santiago.alvarezjulia@gmail.com>
2018-05-17 16:10:46 -0300

    Algunas correcciones a errores

--- TP2/cache_santi/cache.c
+++ TP2/cache_santi/cache.c
@@ -13,45 +13,38 @@
 #define INDEX_MASK 0x1E0
 #define WORD_OFFSET_MASK 0x1C
 #define BLOCK_OFFSET_MASK 0x3
-
-unsigned char* create_memory() {
-    unsigned char* v = (unsigned char*) malloc(MEMORY_SIZE);
-    return v;
-}
-
+#include <stdio.h>
 
 cache_block_t* create_cache_block() {
     cache_block_t* cache_block_ = (cache_block_t*) malloc(sizeof(cache_block_t));;
-    unsigned char *v = (unsigned char*) malloc(BLOCK_SIZE);
-    
     cache_block_->LRU_list = lista_crear();
-    cache_block_->data = v;
+    cache_block_->data = (unsigned char*) malloc(BLOCK_SIZE);;
     cache_block_->V = 0; //Valido es 1 e invalido es 0
     cache_block_->tag = 0;
     return cache_block_;
 }
 
-//Memoria principal -variable global
-unsigned char *memory;
 
-//Memoria cache -variable global
-cache_t *cache_memory;
-
-void init() {
-	memory = create_memory();
+void init(cache_t* cache_memory) {
+	cache_memory->memory = (unsigned char*) malloc(MEMORY_SIZE);
     cache_memory->misses = 0;
     cache_memory->accesses = 0;
+    
+    cache_memory->cache_1st_way = (cache_block_t**) malloc(sizeof(cache_block_t*) * BLOCKS_PER_WAY);
+    cache_memory->cache_2nd_way = (cache_block_t**) malloc(sizeof(cache_block_t*) * BLOCKS_PER_WAY);
     for (int i = 0; i < BLOCKS_PER_WAY; ++i) {
         cache_memory->cache_1st_way[i] = create_cache_block();
         cache_memory->cache_2nd_way[i] = create_cache_block();
     }
 }
 
+
 unsigned char get_data(cache_block_t* block, int word_offset, int block_offset) {
 	return block->data[4 * word_offset + block_offset];
 }
 
-unsigned char read_byte(int address) {
+
+unsigned char read_byte(cache_t* cache_memory, int address) {
 	int tag = address & TAG_MASK;
 	// shift right tag 9 bits
 	tag = tag >> 9;
@@ -68,60 +61,47 @@ unsigned char read_byte(int address) {
 	
 	cache_block_t* block_1st_way = cache_memory->cache_1st_way[index];
 	cache_block_t* block_2nd_way = cache_memory->cache_2nd_way[index];
-	unsigned char data = '1';
+	unsigned char data = 1; // en el enuciado decia -1 si no esta en cache pero no se puede hacer
 	cache_memory->accesses++;
 	
 	if (tag == block_1st_way->tag) {
 		if (block_1st_way->V == 1) {
 			// hit
+			printf("HIT 1!\n");
 			data = get_data(block_1st_way, word_offset, block_offset);
 		} else {
-			// miss, cargar bloque, actualizar tag y bit V
-			unsigned char memory_data = memory[address];
-			
-			if (block_1st_way->LRU_list->lista_largo == 32) {
-				// esta llena. Reemplazar el primero que enliste
-				
-			} else {
-				block_1st_way->data[4 * word_offset + block_offset] = memory_data;
-			}
+			// miss, cargar bloque, actualizar bit V
+			//unsigned char memory_data = memory[address];
+			printf("MISS 1!\n");
 			block_1st_way->V = 1;
 			cache_memory->misses++;
 		}
 	} else if (tag == block_2nd_way->tag) {
 		if (block_1st_way->V == 1) {
 			// hit
+			printf("HIT 2!\n");
 			data = get_data(block_1st_way, word_offset, block_offset);
 		} else {
-			// miss, cargar bloque, actualizar tag y bit V
-			unsigned char memory_data = memory[address];
-			
-			if (block_1st_way->LRU_list->lista_largo == 32) {
-				// esta llena
-				
-			} else {
-				block_1st_way->data[4 * word_offset + block_offset] = memory_data;
-			}
+			// miss, cargar bloque, actualizar bit V
+			//unsigned char memory_data = memory[address];
+			printf("MISS 2!\n");
 			block_1st_way->V = 1;
 			cache_memory->misses++;
 		}
 	} else {
 		// miss, cargar bloque, actualizar tag y bit V
-		unsigned char memory_data = memory[address];
-		if (block_1st_way->LRU_list->lista_largo == 32) {
-			// esta llena
-				
-		} else {
-			block_1st_way->data[4 * word_offset + block_offset] = memory_data;
-		}
+		//unsigned char memory_data = memory[address];
+		printf("MISS 3!\n");
+		block_1st_way->tag = (unsigned char)tag;
+		block_1st_way->V = 1;
 		cache_memory->misses++;
 	}
-	
+
 	return data;
 }
 
 
-int write_byte(int address, unsigned char value) {
+int write_byte(cache_t* cache_memory, int address, unsigned char value) {
 	int tag = address & TAG_MASK;
 	// shift right tag 9 bits
 	tag = tag >> 9;
@@ -145,12 +125,12 @@ int write_byte(int address, unsigned char value) {
 		if (tag == block_1st_way->tag) {
 			// el dato ya fue almacenado en cache. Lo almaceno en memoria ppal.
 			// y actualizo el dato en cache
-			memory[address] = value;
+			cache_memory->memory[address] = value;
 			block_1st_way->data[4 * word_offset + block_offset] = value;
 			is_a_cache_miss = 0;
 		} else {
 			// el dato no fue almacenado en cache. Lo almaceno en memoria ppal.
-			memory[address] = value;
+			cache_memory->memory[address] = value;
 			is_a_cache_miss = -1;
 		}
 	} else if (block_2nd_way->V == 1) {
@@ -158,17 +138,17 @@ int write_byte(int address, unsigned char value) {
 		if (tag == block_2nd_way->tag) {
 			// el dato ya fue almacenado en cache. Lo almaceno en memoria ppal.
 			// y actualizo el dato en cache
-			memory[address] = value;
+			cache_memory->memory[address] = value;
 			block_1st_way->data[4 * word_offset + block_offset] = value;
 			is_a_cache_miss = 0;
 		} else {
 			// el dato no fue almacenado en cache. Lo almaceno en memoria ppal.
-			memory[address] = value;
+			cache_memory->memory[address] = value;
 			is_a_cache_miss = -1;
 		}
 	} else {
 		// el dato no fue almacenado
-		memory[address] = value;
+		cache_memory->memory[address] = value;
 		is_a_cache_miss = -1;
 	}
 	
@@ -176,18 +156,21 @@ int write_byte(int address, unsigned char value) {
 }
 
 
-unsigned int get_miss_rate() {
-	return ((cache_memory->misses / cache_memory->accesses) * 100);
+unsigned int get_miss_rate(cache_t* cache_memory) {
+	return (unsigned int)((float)((float)cache_memory->misses / (float)cache_memory->accesses) * 100);
 }
 
 
-void destroy_cache() {
+void destroy_cache(cache_t* cache_memory) {
 	for (int i = 0; i < BLOCKS_PER_WAY; ++i) {
 		lista_destruir(cache_memory->cache_1st_way[i]->LRU_list, NULL);
 		lista_destruir(cache_memory->cache_2nd_way[i]->LRU_list, NULL);
+        free(cache_memory->cache_1st_way[i]->data);
+        free(cache_memory->cache_2nd_way[i]->data);
         free(cache_memory->cache_1st_way[i]);
         free(cache_memory->cache_2nd_way[i]);
     }
-	
-	free(memory);
+	free(cache_memory->cache_1st_way);
+	free(cache_memory->cache_2nd_way);
+	free(cache_memory->memory);
 }
--- TP2/cache_santi/cache.h
+++ TP2/cache_santi/cache.h
@@ -16,13 +16,15 @@ typedef struct cache {
 	cache_block_t **cache_1st_way; 
 	cache_block_t **cache_2nd_way; 
 	
+	unsigned char* memory;
+	
 	unsigned int misses;
 	unsigned int accesses;
 }cache_t;
 
-void init();
-unsigned char read_byte(int address);
-int write_byte(int address, unsigned char value);
+void init(cache_t* cache_memory);
+unsigned char read_byte(cache_t* cache_memory, int address);
+int write_byte(cache_t* cache_memory, int address, unsigned char value);
 unsigned int get_miss_rate();
 void destroy_cache();
 
